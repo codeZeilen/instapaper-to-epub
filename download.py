498ce429a07c70fbf80a52b537daa97b6978f83e
Patrick R <hi@patrickrein.de>
2024-01-06 11:21:27 +0100

    Refactors file to make the steps stand out more clearly

--- download.py
+++ download.py
@@ -11,23 +11,110 @@ import io
 import mimetypes as mime
 from urllib.parse import urlparse
 
-# Replace with your Instapaper credentials
 with open("oauth_config.json", "r") as f:
     oauth_config = json.load(f)
-
 with open("user_credentials.json", "r") as f:
     user_credentials = json.load(f)
+instapaper = Instapaper(oauth_config['id'], oauth_config['secret'])
+instapaper.login(user_credentials['username'], user_credentials['password'])
 
 if len(sys.argv) > 1:
     num_bookmarks_to_retrieve = sys.argv[1]
 else:
     num_bookmarks_to_retrieve = 70
- 
-instapaper = Instapaper(oauth_config['id'], oauth_config['secret'])
-instapaper.login(user_credentials['username'], user_credentials['password'])
 
 books_folder = Path('books')
 
+def download():
+    for bookmark in instapaper.bookmarks(limit=num_bookmarks_to_retrieve):
+        if not get_content(bookmark):
+            continue
+
+        book = create_book(bookmark)
+        sanitize_and_add_images(bookmark, book)
+        cover = create_and_add_cover(bookmark, book)
+        chapter = create_and_add_chapter(bookmark, book)
+        add_navigation_files(book, cover, chapter)
+        write_book(bookmark, book)
+
+#
+# Downloading content
+#
+def get_content(bookmark):
+    adapt_title_and_file_name(bookmark)        
+
+    if bookmark_already_downloaded(bookmark):
+        print(f'Skipping {bookmark.original_title}, as corresponding book already exists.')
+        return None
+    
+    print(f'Downloading {bookmark.title}')
+    get_and_sanitize_content(bookmark)
+    if not bookmark.sanitized_content:
+        print(f'Skipping {bookmark.title} due to empty content.')
+        return None
+
+    return bookmark
+
+def adapt_title_and_file_name(bookmark):
+    bookmark.original_title = bookmark.title
+    bookmark.title = 'Instapaper: ' + bookmark.title
+    bookmark.book_file_name = make_safe_filename(bookmark.title)
+
+def bookmark_already_downloaded(bookmark):
+    return (books_folder / f'{bookmark.book_file_name}.epub').exists()
+
+def get_and_sanitize_content(bookmark):
+    bookmark.sanitized_content = bookmark.html
+    if bookmark.sanitized_content:
+        bookmark.sanitized_content = bookmark.sanitized_content.strip()
+
+
+#
+# EPub Creation
+#
+def create_book(bookmark):
+    book = epub.EpubBook()
+    book.set_identifier(str(bookmark.bookmark_id))
+    book.set_title(bookmark.title)
+    return book
+
+def create_and_add_cover(bookmark, book):
+    cover_content = f'<html><body><h1 style="width: 70%; margin: 0 auto;">{bookmark.original_title}</h1></body></html>'
+    cover = epub.EpubHtml(
+        title=bookmark.title, 
+        file_name=f'{bookmark.bookmark_id}_cover.xhtml')
+    cover.content = cover_content
+    book.add_item(cover)
+    return cover
+
+def create_and_add_chapter(bookmark, book):
+    chapter = epub.EpubHtml(
+        title=bookmark.title, 
+        file_name=f'{bookmark.bookmark_id}.xhtml')
+    chapter.content = bookmark.sanitized_content
+    book.add_item(chapter)
+    return chapter
+
+def sanitize_and_add_images(bookmark, book):
+    bookmark.sanitized_content = shrink_replace_and_add_images(bookmark.sanitized_content, book)
+
+def add_navigation_files(book, cover, chapter):
+    book.toc = (cover,chapter,)
+    
+    # Add navigation files
+    book.add_item(epub.EpubNcx())
+    book.add_item(epub.EpubNav())
+    
+    # Define the spine of the book
+    book.spine = [cover, chapter,]
+
+def write_book(bookmark, book):
+    epub.write_epub(f'./books/{bookmark.book_file_name}.epub', book, {})
+
+
+#
+# Sanitizing content
+#
 def make_safe_filename(s):
     # Define a whitelist of characters that are allowed in filenames
     valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
@@ -39,6 +126,39 @@ def make_safe_filename(s):
     safe_filename = safe_filename.replace(' ', '_')
 
     return safe_filename
+
+def shrink_replace_and_add_images(html, book):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    images = soup.find_all('img')
+    images = filter(lambda i: i.has_attr('src') and not i['src'].startswith('data:'), images)
+    for img in images:  
+        url_file_name = urlparse(img['src']).path.split('/')[-1]
+        filename = f'./tmp_images/{url_file_name}'
+        filepath = Path(filename)
+        
+        if (not filepath.suffix) or (filepath.suffix in ('.html', '.svg', '.webp')):
+            replace_img_with_alt_text(img, soup)
+            continue
+        
+        image_data = None
+        try:
+            response = requests.get(img['src'])
+            if response.status_code == 200:
+                image_data = response.content
+        except Exception:
+            pass
+
+        if image_data:
+            image_data = convert_image(image_data, filename)
+            add_image_to_book(book, url_file_name, image_data)
+
+            # Update the img tag in the HTML
+            img['src'] = url_file_name
+        else:
+            replace_img_with_alt_text(img, soup)
+
+    return str(soup)
 
 def replace_imgs_with_alt_text(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
@@ -58,108 +178,25 @@ def replace_img_with_alt_text(img, soup):
         return True
     else:
         return False
+    
+def convert_image(image_data, filename):
+    # Dither the image to save space
+    image = Image.open(io.BytesIO(image_data))
+    image = image.convert('1', dither=Image.FLOYDSTEINBERG)
+    image.save(filename)
 
-def shrink_and_replace_images(html, book):
-    # Parse the HTML
-    soup = BeautifulSoup(html, 'html.parser')
+    with open(filename, 'rb') as f:
+        return f.read()
 
-    for img in soup.find_all('img'):
-        img_url = img['src']
-        if img_url.startswith('data:'):
-            # Nothing to do here
-            continue
-        
-        parsed_url = urlparse(img_url)
-        url_file_name = parsed_url.path.split('/')[-1]
-        filename = f'./tmp_images/{url_file_name}'
-        filepath = Path(filename)
-        if (not filepath.suffix) or (filepath.suffix in ('.html', '.svg', '.webp')):
-            replace_img_with_alt_text(img, soup)
-            continue
-        
-        try:
-            response = requests.get(img_url)
-            if not response.status_code == 200:
-                replace_img_with_alt_text(img, soup)
-            else: 
-                img_data = response.content
-                            
-                # Dither the image to save space
-                image = Image.open(io.BytesIO(img_data))
-                image = image.convert('1', dither=Image.FLOYDSTEINBERG)
-                image.save(filename)
-            
-                with open(filename, 'rb') as f:
-                    image_data = f.read()
+def add_image_to_book(book, url_file_name, image_data):
+    img_path = Path(url_file_name)
+    book_image = epub.EpubItem(uid=img_path.name, 
+        file_name=url_file_name, 
+        media_type=mime.guess_type(url_file_name)[0], 
+        content=image_data)
+    book.add_item(book_image)
+    return url_file_name
 
-                img_path = Path(url_file_name)
-                book_image = epub.EpubItem(uid=img_path.name, 
-                    file_name=url_file_name, 
-                    media_type=mime.guess_type(url_file_name)[0], 
-                    content=image_data)
-                book.add_item(book_image)
-
-                # Update the img tag in the HTML
-                img['src'] = url_file_name
-        except Exception:
-            replace_img_with_alt_text(img, soup)
-
-    return str(soup)
-
-# Get all bookmarks
-for bookmark in instapaper.bookmarks(limit=num_bookmarks_to_retrieve):
-    bookmark.original_title = bookmark.title
-    bookmark.title = 'Instapaper: ' + bookmark.title
-    book_file_name = make_safe_filename(bookmark.title)
-    if not book_file_name:
-        continue 
-    if not (books_folder / f'{book_file_name}.epub').exists():
-        print(f'Downloading {bookmark.title}')
-
-        # Check whether content is available
-        content = bookmark.html
-        if not (content and content.strip()):
-            print(f'Skipping {bookmark.title} due to empty content')
-            continue
-
-        # Create an epub book
-        book = epub.EpubBook()
-        book.set_identifier(str(bookmark.bookmark_id))
-        book.set_title(bookmark.title)
-        book.set_language('en')
-
-        # Create an epub chapter with the cover
-        
-        ## Create the Cover
-        cover_content = f'<html><body><h1 style="width: 70%; margin: 0 auto;">{bookmark.original_title}</h1></body></html>'
-        
-        ## Add Cover
-        cover = epub.EpubHtml(title=bookmark.title, file_name=f'{bookmark.bookmark_id}_cover.xhtml', lang='en')
-        cover.content = cover_content
-        book.add_item(cover)
-        
-        # Create an epub chapter from the article content
-
-        ## Prepare Content
-        content = shrink_and_replace_images(content, book)
-
-        ## Add Chapter
-        chapter = epub.EpubHtml(title=bookmark.title, file_name=f'{bookmark.bookmark_id}.xhtml', lang='en')
-        chapter.content = content
-        book.add_item(chapter)
-        
-        # Add the chapter to the book's table of contents
-        book.toc = (cover,chapter,)
-        
-        # Add navigation files
-        book.add_item(epub.EpubNcx())
-        book.add_item(epub.EpubNav())
-        
-        # Define the spine of the book
-        book.spine = [cover, chapter,]
-        
-        # Write the epub file to disk
-        epub.write_epub(f'./books/{book_file_name}.epub', book, {})
-    else:
-        print(f'Skipping {bookmark.title}')
-
+# Download ebooks when started as a script
+if __name__ == '__main__':
+    download()
